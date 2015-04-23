12379e571815735c6b46930879946122e2fb14ed
Gabe Pressman <pressman4747@gmail.com>
2015-04-23 17:00:57 -0400

    finished window table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421215000) do
+ActiveRecord::Schema.define(version: 20150423005634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,15 @@ ActiveRecord::Schema.define(version: 20150421215000) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "windows", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "windows", ["recipient_id"], name: "index_windows_on_recipient_id", using: :btree
+  add_index "windows", ["sender_id"], name: "index_windows_on_sender_id", using: :btree
 
 end
